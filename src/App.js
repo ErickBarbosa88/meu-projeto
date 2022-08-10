f0dbfed2cad82c408dab5da03ea296bbd9d8bf0e
ErickBarbosa88 <barbosaerick88@gmail.com>
2022-08-10 19:59:41 -0300

    aula1008

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Titulo } from './components/Titulo';
+
 
 function App() {
   let idade = 23;
   return (
       <div>
     <div className="App">
+      <Titulo txt="Nome: Maria"/>
+      <Titulo txt={`Idade = ${idade}`}/>
       <h1>Olá mundo, idade = {idade}</h1>
+      <h1>ola</h1>
     </div>
     <div>DIV 2
-      
+
     </div>
     </div>
   );
