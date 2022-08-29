ab34fed24a3f6ffaf6b96105bc517fa3fbe8d159
ErickBarbosa88 <barbosaerick88@gmail.com>
2022-08-29 20:02:34 -0300

    ContadorRegex

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import './App.css';
-import React from 'react';
-import { Titulo } from './components/Titulo';
+import React, {useState} from "react";
+import "./App.css";
+
+
+
+function getVowels(str) {
+  var m = str.match (/[aeioãuíêôàú]/gi);
+  return m === null ? 0 : m.length;
+  
+}
+
+function getConso(str) {
+  var c = str.match (/[bcçdqwrtypsdfghjklzxcvbnm]/gi);
+  return c === null ? 0 : c.length;
+  
+}
 
 function App() {
-  let idade = 23;
-  return (
-      <div>
-    <div className="App">
-      <Titulo txt="Nome: Maria"/>
-      <Titulo txt={`Idade = ${idade}`}/>
-      <h1>Olá mundo, idade = {idade}</h1>
-      <h1>Hello World</h1>
-      <h2>Teste</h2>
-      <h2>Teste</h2>
-    </div>
-    <div>DIV 2
+  const [texto, setTexto] = useState("");
+  const handleChangeInputTexto = (evt) => {
+    setTexto(evt.target.value);
+  }
 
-    </div>
+  return (
+  <div className="App">
+    <input type="text" value={texto} onChange={handleChangeInputTexto}/>
+    <h3>Vogais: {getVowels(texto)}</h3>
+    <h3>Consoantes: {getConso(texto)}</h3>
+
+  </div>
+  )
+
+}
+export default App;
+
+/*import React, { useState } from "react";
+import { ContadorRegex } from "./components/ContadorRegex";
+import "./App.css";
+
+function App() {
+  const regexVogais = /[aáàãâeéêiíoóõôuú]/gi;
+  const regexConsoantes = /[bcçdfghjklmnpqrstvwxyz]/gi;
+  const [texto, setTexto] = useState("");
+  const handleChangeInputTexto = (evt) => {
+    setTexto(evt.target.value);
+  };
+
+  return (
+    <div className="App">
+      <input type="text" value={texto} onChange={handleChangeInputTexto} />
+      <ContadorRegex titulo="Vogais" texto={texto} regex={regexVogais} />
+      <ContadorRegex
+        titulo="Consoantes"
+        texto={texto}
+        regex={regexConsoantes}
+      />
     </div>
   );
 }
 
-export default App;
+export default App;*/
